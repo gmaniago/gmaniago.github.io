3908612b5f4ccfc18aa4caf2c811966209dac470
Gladys Maniago <gfmaniago@gmail.com>
2016-01-13 23:37:59 -0800

    adds email link to resume page

---
+++ exercise1.js
@@ -0,0 +1,20 @@
+// Define a function max() that takes two numbers as arguments and 
+// returns the largest of them. Use the if-then-else construct available 
+// in Javascript.
+
+function max(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+// Define a new function named max that takes two parameters
+// If the first parameter has a value greater than the second parameter
+// Then this function will return the first parameter
+// Close the ‘if’ clause
+// Otherwise (meaning if second parameter is greater than first)
+// Then this function will return the second parameter
+// Close the ‘else’ clause
+// Close the function definition
